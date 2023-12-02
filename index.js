934f2a0b1e386156a7fe05adc4bd4ab87f8398c0
a7ndriyyy <andrii.nastych@gmail.com>
2023-12-02 12:59:07 +0100

    commit

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const {
   removeContact,
 } = require("./contacts");
 
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
 const argv = require("yargs").argv;
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
